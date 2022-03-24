45266c23bd65920bc1d20a53922c9373cbcea9c2
elena-nikulina <info@webstar-support.com>
2022-03-24 16:47:51 +0700

    Второй коммит

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const mycode = 1;
 const myFunction = () => {
   console.log(mycode);
 };
+
+let title=" ";
+let screens=1;
+let screenPrice=0;
+let rollback=" ";
+let fullPrice=1000000;
+let adaptive='yes';
+
+alert("Первое д.з.");
+console.log('First homework');
