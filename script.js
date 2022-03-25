fbfa4680dd81d693581a42ccc2652fac3caaf106
elena-nikulina <info@webstar-support.com>
2022-03-25 12:49:23 +0700

    Выполнила д.з. ко второму уроку

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const mycode = 1;
-
-const myFunction = () => {
-  console.log(mycode);
-};
-
-let title=" ";
-let screens=1;
-let screenPrice=0;
-let rollback=" ";
+let title="project";
+let screens="Простые, Сложные, Интерактивные";
+let screenPrice=10;
+let rollback=99;
 let fullPrice=1000000;
-let adaptive='yes';
+let adaptive=true;
 
-alert("Первое д.з.");
-console.log('First homework');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + (screenPrice) + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + (fullPrice) + ' рублей/ долларов/гривен/юани');
+
+let scr=screens.toLowerCase();
+let arr=scr.split('');
+console.log(arr);
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
+
 
