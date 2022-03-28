970564f845557a9bdda1e81308958f48d3582b6e
elena-nikulina <info@webstar-support.com>
2022-03-28 21:24:42 +0700

    Выполнила простое задание к уроку 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let title="project";
 let screens="Простые, Сложные, Интерактивные";
 let screenPrice=10;
@@ -16,4 +18,30 @@ console.log('Стоимость разработки сайта ' + (fullPrice) 
 console.log((screens.toLowerCase()).split(', '));
 console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
 
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+
+switch(true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    default: 
+        console.log('Что-то пошло не так');
+        break;
+}
